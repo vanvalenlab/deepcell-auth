4148e7759f38564492094c937178cc12269aab98
Ross Barnowski <rossbar@caltech.edu>
2026-06-03 18:25:53 -0700

    ENH: Basic infrastructure for a deepcell-wide asset manifest
    Create a yaml manifest for pretrained weights/datasets.
    
    Use cellsam assets as proof-of-concept

--- deepcell_auth/__init__.py
+++ deepcell_auth/__init__.py
@@ -1,1 +1,3 @@
 __version__ = "0.1.0-dev"
+
+from ._download import *
---
+++ deepcell_auth/_download.py
@@ -0,0 +1,73 @@
+import importlib
+import yaml
+
+
+__all__ = [
+    "download_cellsam_evaluation_dataset",
+    "download_cellsam_model",
+]
+
+def download_cellsam_model(version=None):
+    """Download pre-trained weights for the CellSAM model.
+
+    Parameters
+    ----------
+    version : str, optional, default=latest
+       Which version of the model weights to download. If not specified, the latest
+       published version will be downloaded. Available versions:
+
+         - 1.2 (latest)
+         - 1.0
+    """
+    from ._auth import fetch_data
+
+    # Load manifest
+    manifest_path = importlib.resources.files("deepcell_auth") / "asset_manifest.yaml"
+    with open(manifest_path) as fh:
+        manifest = yaml.safe_load(fh.read())["models"]["cellsam"]
+
+    version = "1.2" if version is None else version
+    try:
+        record = manifest[version]
+    except KeyError:
+        raise KeyError(
+            f"Version {version} not found. Available versions: {list(manifest)}"
+        )
+
+    fetch_data(
+        record["asset_key"], cache_subdir="models", file_hash=record["asset_hash"]
+    )
+
+def download_cellsam_evaluation_dataset(version=None):
+    """Download the evaluation data for the CellSAM model.
+
+    The compressed dataset will be downloaded to the canonical location:
+    ``$HOME/.deepcell/data``.
+
+    Parameters
+    ----------
+    version : str, optional, default=latest
+       Which version of the dataset to download. If not specified, the latest
+       published version will be downloaded. Available versions:
+
+         - 1.2 (latest)
+         - 1.0
+    """
+    from ._auth import fetch_data
+
+    # Load manifest
+    manifest_path = importlib.resources.files("deepcell_auth") / "asset_manifest.yaml"
+    with open(manifest_path) as fh:
+        manifest = yaml.safe_load(fh.read())["datasets"]["cellsam"]
+
+    version = "1.2" if version is None else version
+    try:
+        record = manifest[version]
+    except KeyError:
+        raise KeyError(
+            f"Version {version} not found. Available versions: {list(manifest)}"
+        )
+
+    fetch_data(
+        record["asset_key"], cache_subdir="data", file_hash=record["asset_hash"]
+    )
