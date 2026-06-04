3c99352159833306fd94398e214f301cff3299d5
Ross Barnowski <rossbar@caltech.edu>
2026-06-03 20:40:10 -0700

    Add deepcell-types pretrained model weights.

--- deepcell_auth/_download.py
+++ deepcell_auth/_download.py
@@ -1,7 +1,9 @@
 __all__ = [
     "download_cellsam_evaluation_dataset",
     "download_cellsam_model",
+    "download_deepcell_types_model",
 ]
+
 
 def download_cellsam_model(version=None):
     """Download pre-trained weights for the CellSAM model.
@@ -25,6 +27,36 @@ def download_cellsam_model(version=None):
     except KeyError:
         raise KeyError(
             f"Version {version} not found. Available versions: {list(manifest)}"
+        )
+
+    fetch_data(
+        record["asset_key"], cache_subdir="models", file_hash=record["asset_hash"]
+    )
+
+
+def download_deepcell_types_model(version=None):
+    """Download pre-trained weights for the deepcell-types model.
+
+    Parameters
+    ----------
+    version : str, optional, default=latest
+       Which version of the model weights to download. If not specified, the latest
+       published version will be downloaded. Available versions:
+
+         - '2025-06-09' (latest)
+         - '2025-06-09_public-data-only'
+    """
+    from ._auth import load_manifest, fetch_data
+
+    manifest = load_manifest()
+    dct_models = manifest["models"]["deepcell-types"]
+
+    version = "2025-06-09" if version is None else version
+    try:
+        record = dct_models[version]
+    except KeyError:
+        raise KeyError(
+            f"Version {version} not found. Available versions: {list(dct_models)}"
         )
 
     fetch_data(
